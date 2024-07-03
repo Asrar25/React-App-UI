0f9ab22c95995faefee3b99919c581a0b65cae3a
Asrar A <114174920+Asrar25@users.noreply.github.com>
2024-07-03 19:08:58 +0530

    Update AdditionalQualification.js

--- src/Components/AdditionalQualification.js
+++ src/Components/AdditionalQualification.js
@@ -20,7 +20,7 @@ export default function AdditionalQualification({ additionalSkill,setAdditionalS
     <form class="bg-white shadow-md rounded px-4 pt-6 pb-8 mb-4">
         <div className="px-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">PLEASE IDENTIFY ANY ADDITIONAL KNOWLEDGE,SKILLS,QUALIFICATION OR AWARDS THAT WILL BE HELPDUL TOMAS IN CONSIDERING YOUR APPLICATION FOR EMPLOYEMENT</label>
-            <textarea rows="4" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none" name="Qualification" onChange={handleinput}></textarea>
+            <textarea rows="4" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none" name="Qualification" onChange={handleinput} value={additionalSkill.Qualification}></textarea>
         </div>
         <div class="flex items-center justify-between mt-4">
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={preStep}>Back</button>
