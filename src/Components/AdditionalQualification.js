f0d223b6ef5d47a9bbaa9e2b51230a23ccdbc5f2
Asrar25 <asaasrar786@gmail.com>
2024-07-04 18:53:49 +0530

    Thursday Work

--- src/Components/AdditionalQualification.js
+++ src/Components/AdditionalQualification.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-<<<<<<< HEAD
 export default function AdditionalQualification({ additionalSkill, setAdditionalSkill, preStep, nextStep }) {
   const [errors, setErrors] = useState({});
 
@@ -26,29 +25,12 @@ export default function AdditionalQualification({ additionalSkill, setAdditional
 
 
 
-=======
-export default function AdditionalQualification({ additionalSkill,setAdditionalSkill, preStep, nextStep }){
-  const [errors, setErrors] = useState({});
-  
-  const handleinput=(e)=>{
-    setAdditionalSkill({
-      ...additionalSkill,
-      [e.target.name]: e.target.value
-  });
-  }
-  const handlesubmit=(e)=>{
-    console.log(additionalSkill);
-  nextStep();
-  }
-  
->>>>>>> d9f5586dbdbc9a61d22fdc63eb906a628e72bc42
   return (
 
 
     <div class="px-20">
       <form class="bg-white shadow-md rounded px-4 pt-6 pb-8 mb-4">
         <div className="px-4">
-<<<<<<< HEAD
           <label className="block text-gray-700 text-sm font-bold mb-2">PLEASE IDENTIFY ANY ADDITIONAL KNOWLEDGE,SKILLS,QUALIFICATION OR AWARDS THAT WILL BE HELPDUL TOMAS IN CONSIDERING YOUR APPLICATION FOR EMPLOYEMENT</label>
           <textarea rows="4" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none" name="Qualification" onChange={handleinput} value={additionalSkill.Qualification}></textarea>
           {errors.Qualification && <p className="text-red-500 text-xs italic">{errors.Qualification}</p>}
@@ -56,14 +38,6 @@ export default function AdditionalQualification({ additionalSkill,setAdditionalS
         <div class="flex items-center justify-between mt-4">
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={preStep}>Back</button>
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handlesubmit}>Next</button>
-=======
-            <label className="block text-gray-700 text-sm font-bold mb-2">PLEASE IDENTIFY ANY ADDITIONAL KNOWLEDGE,SKILLS,QUALIFICATION OR AWARDS THAT WILL BE HELPDUL TOMAS IN CONSIDERING YOUR APPLICATION FOR EMPLOYEMENT</label>
-            <textarea rows="4" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none" name="Qualification" onChange={handleinput} value={additionalSkill.Qualification}></textarea>
-        </div>
-        <div class="flex items-center justify-between mt-4">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={preStep}>Back</button>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handlesubmit}>Next</button>
->>>>>>> d9f5586dbdbc9a61d22fdc63eb906a628e72bc42
         </div>
       </form>
     </div>
--- src/Components/GeneralInfo.js
+++ src/Components/GeneralInfo.js
@@ -11,7 +11,6 @@ function GeneralInfo({formAllData, handleChange, nextStep}) {
 
 
         let errors = {};
-<<<<<<< HEAD
         if (!formAllData.FullName) {
             errors.FullName = "Full Name is required";
         }
@@ -96,92 +95,6 @@ function GeneralInfo({formAllData, handleChange, nextStep}) {
             console.log(errors);
             setErrors(errors);
         }
-=======
-        // if (!formData.FullName) {
-        //     errors.FullName = "Full Name is required";
-        // }
-        // else if (formData.FullName.length < 3) {
-        //     errors.FullName = "Full Name must be at least 3 characters";
-        // }
-        // else if (!/^[a-zA-Z\s]+$/.test(formData.FullName)) {
-        //     errors.FullName = "Full Name can only contain letters and spaces";
-        // }
-        // // Add more validation rules as needed for other fields
-        // if (!formData.PhoneNumber.trim()) {
-        //     errors.PhoneNumber = "Phone Number is required";
-        // } else if (!/^\d{10}$/.test(formData.PhoneNumber.trim())) {
-        //     errors.PhoneNumber = "Phone Number must be exactly 10 digits";
-        // }
-        // if (!formData.FaxNumber.trim()) {
-        //     errors.FaxNumber = "Fax Number is required";
-        // } else if (!/^\+\d{2}\d{10}$/.test(formData.FaxNumber.trim())) {
-        //     errors.FaxNumber = "Fax Number must be in the format '+XX1234567890'";
-        // }
-        // if (!formData.Email.trim()) {
-        //     errors.Email = "Email is required";
-        // } else if (!/\S+@\S+\.\S+/.test(formData.Email.trim())) {
-        //     errors.Email = "Invalid email format";
-        // }
-        // if (!formData.Address.trim()) {
-        //     errors.Address = "Address is required";
-        // }
-
-        // // Position validation
-        // if (!formData.Position.trim()) {
-        //     errors.Position = "Position is required";
-        // }
-
-        // // Present Salary validation
-        // if (!formData.PresentSalary.trim()) {
-        //     errors.PresentSalary = "Present Salary is required";
-        // } else if (!/^\d+(\.\d{1,2})?$/.test(formData.PresentSalary.trim())) {
-        //     errors.PresentSalary = "Present Salary must be a valid number";
-        // }
-
-        // if (!formData.SalaryDesire.trim()) {
-        //     errors.SalaryDesire = "Salary Desire is required";
-        // } else if (!/^\d+(\.\d{1,2})?$/.test(formData.SalaryDesire.trim())) {
-        //     errors.SalaryDesire = "Salary Desire must be a valid number";
-        // }
-
-        // if (!formData.Time.trim()) {
-        //     errors.Time = "Time is required";
-        // }
-
-        // // AlreadySB validation
-        // if (!formData.AlreadySB.trim()) {
-        //     errors.AlreadySB = "This Fiel is required";
-        // }
-        // if (!formData.Application.trim()) {
-        //     errors.Application = "Application is required";
-        // }
-        // if (!formData.LegallyWork.trim()) {
-        //     errors.LegallyWork = "Legally Work is required";
-        // }
-        // if (!formData.SponsorshipEmployement.trim()) {
-        //     errors.SponsorshipEmployement = "Sponsorship Employement is required";
-        // }
-        // if (!formData.ConvictedCrime.trim()) {
-        //     errors.ConvictedCrime = "Convicted Crime is required";
-        // }
-        // if (!formData.Time.trim()) {
-        //     errors.Time = 'Time is required';
-        // }
-        // if (!formData.Date.trim()) {
-        //     errors.Date = "Date is required";
-        // } else {
-        //     const isValidDate = /^\d{4}-\d{2}-\d{2}$/.test(formData.Date.trim());
-        //     if (!isValidDate) {
-        //         errors.Date = "Date must be in the format YYYY-MM-DD";
-        //     }
-        // }
-        if (Object.keys(errors).length == 0) {
-            nextStep();
-        } else {
-            console.log(errors);
-            setErrors(errors);
-        }
->>>>>>> d9f5586dbdbc9a61d22fdc63eb906a628e72bc42
 
     };
 
@@ -251,11 +164,7 @@ function GeneralInfo({formAllData, handleChange, nextStep}) {
 
                 </div>
 
-<<<<<<< HEAD
                 <div className="flex flex-col md:flex-row items-center">
-=======
-                <div className="flex flex-col md:flex-row items-center mb-4">
->>>>>>> d9f5586dbdbc9a61d22fdc63eb906a628e72bc42
                     <div className="w-full md:w-1/2 flex items-center mb-4 md:mb-0 md:mr-4">
                         <label className="ml-2 text-gray-700 text-sm font-bold">
                             <input type="radio" name="Time" value="PartTime" checked={formAllData.Time === 'PartTime'} onChange={handleChange} className="form-radio h-3 w-4 text-blue-600 focus:ring-blue-500 border-gray-300" />PART-TIME</label>
@@ -265,21 +174,12 @@ function GeneralInfo({formAllData, handleChange, nextStep}) {
                         <label className="ml-2 text-gray-700 text-sm font-bold">
                             <input type="radio" name="Time" value="FullTime" checked={formAllData.Time === 'FullTime'} onChange={handleChange} className="form-radio h-3 w-4 text-blue-600 focus:ring-blue-500 border-gray-300" />FULL-TIME</label>
                     </div>
-<<<<<<< HEAD
                 </div>
                 {errors.Time && <p className="text-red-500 text-xs italic mb-4">{errors.Time}</p>}
               
 
                 <div className="flex flex-col md:flex-row items-center">
                     <label className="w-full md:w-auto text-gray-700 text-sm font-bold mb-2 md:mb-0">HAVE YOU EVER APPLIED TO SB & COMPANY, LLC BEFORE</label>
-=======
-                    {errors.Time && <p className="text-red-500 text-xs italic">{errors.Time}</p>}
-                </div>
-
-                <div className="flex flex-col md:flex-row items-center mb-4">
-                    <label className="w-full md:w-auto text-gray-700 text-sm font-bold mb-2 md:mb-0">HAVE YOU EVER APPLIED TO SB & COMPANY, LLC BEFORE</label>
-                    {errors.AlreadySB && <p className="text-red-500 text-xs italic">{errors.AlreadySB}</p>}
->>>>>>> d9f5586dbdbc9a61d22fdc63eb906a628e72bc42
                     <div className="flex items-center">
                         <label className="ml-2 text-gray-700 text-sm font-bold">
                             <input type="radio" name="AlreadySB" value="yes" checked={formAllData.AlreadySB === 'yes'} onChange={handleChange} className="form-radio h-3 w-4 text-blue-600 focus:ring-blue-500 border-gray-300" />YES</label>
@@ -289,13 +189,8 @@ function GeneralInfo({formAllData, handleChange, nextStep}) {
                         <label className="ml-2 text-gray-700 text-sm font-bold">
                             <input type="radio" name="AlreadySB" value="no" checked={formAllData.AlreadySB === 'no'} onChange={handleChange} className="form-radio h-3 w-4 text-blue-600 focus:ring-blue-500 border-gray-300" />NO</label>
                     </div>
-<<<<<<< HEAD
                 </div>
                 {errors.AlreadySB && <p className="text-red-500 text-xs italic mb-4">{errors.AlreadySB}</p>}
-=======
-                    
-                </div>
->>>>>>> d9f5586dbdbc9a61d22fdc63eb906a628e72bc42
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2">IF YES, WHAT?</label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" />
@@ -307,11 +202,7 @@ function GeneralInfo({formAllData, handleChange, nextStep}) {
                     {errors.Application && <p className="text-red-500 text-xs italic">{errors.Application}</p>}
                 </div>
 
-<<<<<<< HEAD
                 <div className="flex flex-col md:flex-row items-center">
-=======
-                <div className="flex flex-col md:flex-row items-center mb-4">
->>>>>>> d9f5586dbdbc9a61d22fdc63eb906a628e72bc42
                     <label className="w-full md:w-auto text-gray-700 text-sm font-bold mb-2 md:mb-0">ARE YOU LEGALLY PERMITTED TO WORK IN THIS COUNTRY?</label>
                     <div className="flex items-center">
                         <label className="ml-2 text-gray-700 text-sm font-bold">
@@ -322,18 +213,11 @@ function GeneralInfo({formAllData, handleChange, nextStep}) {
                         <label className="ml-2 text-gray-700 text-sm font-bold">
                             <input type="radio" name="LegallyWork" value="no" checked={formAllData.LegallyWork === 'no'} onChange={handleChange} className="form-radio h-3 w-4 text-blue-600 focus:ring-blue-500 border-gray-300" />NO</label>
                     </div>
-<<<<<<< HEAD
                    
                 </div>
                 {errors.LegallyWork && <p className="  mb-4 text-red-500 text-xs italic">{errors.LegallyWork}</p>}
 
                 <div className="flex flex-col md:flex-row items-center ">
-=======
-                    {errors.LegallyWork && <p className="text-red-500 text-xs italic">{errors.LegallyWork}</p>}
-                </div>
-
-                <div className="flex flex-col md:flex-row items-center mb-4">
->>>>>>> d9f5586dbdbc9a61d22fdc63eb906a628e72bc42
                     <label className="w-full md:w-auto text-gray-700 text-sm font-bold mb-2 md:mb-0">WILL YOU NOW OR IN THE FUTURE REQUIRE SPONSORSHIP FOR EMPLOYMENT VISA STATUS?</label>
                     <div className="flex items-center">
                         <label className="ml-2 text-gray-700 text-sm font-bold">
@@ -344,18 +228,11 @@ function GeneralInfo({formAllData, handleChange, nextStep}) {
                         <label className="ml-2 text-gray-700 text-sm font-bold">
                             <input type="radio" name="SponsorshipEmployement" value="no" checked={formAllData.SponsorshipEmployement === 'no'} onChange={handleChange} className="form-radio h-3 w-4 text-blue-600 focus:ring-blue-500 border-gray-300" />NO</label>
                     </div>
-<<<<<<< HEAD
                     
                 </div>
                 {errors.SponsorshipEmployement && <p className="text-red-500 text-xs italic mb-4">{errors.SponsorshipEmployement}</p>}
 
                 <div className="flex flex-col md:flex-row items-center ">
-=======
-                    {errors.SponsorshipEmployement && <p className="text-red-500 text-xs italic">{errors.SponsorshipEmployement}</p>}
-                </div>
-
-                <div className="flex flex-col md:flex-row items-center mb-4">
->>>>>>> d9f5586dbdbc9a61d22fdc63eb906a628e72bc42
                     <label className="w-full md:w-auto text-gray-700 text-sm font-bold mb-2 md:mb-0">HAVE YOU EVER BEEN CONVICTED OF A CRIME OTHER THAN A MINOR TRAFFIC VIOLATION?</label>
                     <div className="flex items-center">
                         <label className="ml-2 text-gray-700 text-sm font-bold">
@@ -366,14 +243,9 @@ function GeneralInfo({formAllData, handleChange, nextStep}) {
                         <label className="ml-2 text-gray-700 text-sm font-bold">
                             <input type="radio" name="ConvictedCrime" value="no" checked={formAllData.ConvictedCrime === 'no'} onChange={handleChange} className="form-radio h-3 w-4 text-blue-600 focus:ring-blue-500 border-gray-300" />NO</label>
                     </div>
-<<<<<<< HEAD
                    
                 </div>
                 {errors.ConvictedCrime && <p className="text-red-500 text-xs italic mb-4">{errors.ConvictedCrime}</p>}
-=======
-                    {errors.ConvictedCrime && <p className="text-red-500 text-xs italic">{errors.ConvictedCrime}</p>}
-                </div>
->>>>>>> d9f5586dbdbc9a61d22fdc63eb906a628e72bc42
 
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold">IF YES, GIVE DETAILS ON A SEPARATE PAGE AND ATTACH TO THE APPLICATION</label>
@@ -387,4 +259,4 @@ function GeneralInfo({formAllData, handleChange, nextStep}) {
 }
 
 
-export default GeneralInfo;
+export default GeneralInfo;eneralInfo;
